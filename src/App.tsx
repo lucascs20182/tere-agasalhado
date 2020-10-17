d9a5e2b2ff0928019fe5648eb210c9d4e80e607f
lucascs20182 <lucascs20182@gmail.com>
2020-10-17 00:07:24 -0300

    modify style to introduce mobile first

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { FiArrowRight } from 'react-icons/fi';
 
 import './styles/global.css';
+import './styles/animations.css';
 import './styles/pages/landing.css';
 
 import logoImg from './assets/mark.png';
@@ -9,30 +10,36 @@ import logoImg from './assets/mark.png';
 function App() {
   return (
     <div id="page-landing">
-      <div className="content-wrapper">
-        <div className="logo">
-          <img src={logoImg} alt="" />
-          <p>Terê Agasalhado</p>
-        </div>
+      <div id="content-wrapper">
+        <header>
+          <div className="logo animate-up">
+            <img src={logoImg} alt="Logomarca do Terê Agasalhado" />
+            <p>Terê Agasalhado</p>
+          </div>
+
+          <div className="location animate-up">
+            <strong>Teresópolis</strong>
+            <span>Rio de Janeiro</span>
+          </div>
+        </header>
 
         <main>
-          <h1>Seu coração pode aquecer Teresópolis. Doe roupas, cobertores e agasalhos.</h1>
-          <p>
-            As doações serão entregues a moradores de rua, orfanatos 
-            e a casas de repouso para idosos.
-          </p>
+          <h1 className="animate-up">
+            Seu coração pode aquecer Teresópolis. Doe roupas, cobertores e agasalhos.
+          </h1>
+          
+          <section>
+            <p className="animate-up">
+              As doações serão entregues a moradores de rua, orfanatos 
+              e a casas de repouso para idosos.
+            </p>
+
+            <a href="#" title="Veja os pontos de coleta"
+              className="enter-app animate-up">
+              <FiArrowRight size={26} color="rgba(0, 0, 0, .6)" />
+            </a>
+          </section>
         </main>
-
-        <div className="location">
-          <strong>Teresópolis</strong>
-          <span>Rio de Janeiro</span>
-        </div>
-
-        
-        <a href="" className="enter-app">
-          <FiArrowRight size={26} color="rgba(0, 0, 0, .6)" />
-        </a>
-        
       </div>
     </div>
   );
