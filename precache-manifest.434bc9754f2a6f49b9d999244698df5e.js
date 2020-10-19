5de571d39ccd331d1738496ce848b0b2a45b3e02
lucascs20182 <lucascs20182@gmail.com>
2020-10-19 11:07:13 -0300

    Updates

---
+++ precache-manifest.434bc9754f2a6f49b9d999244698df5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b40c67501a2dd777cc3ebd3eba19186a",
+    "url": "/tere-agasalhado-web/index.html"
+  },
+  {
+    "revision": "86b5e148d8a0451189d3",
+    "url": "/tere-agasalhado-web/static/css/main.7b160026.chunk.css"
+  },
+  {
+    "revision": "eeaf9c1c41b0b315b77c",
+    "url": "/tere-agasalhado-web/static/js/2.1abddba4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tere-agasalhado-web/static/js/2.1abddba4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86b5e148d8a0451189d3",
+    "url": "/tere-agasalhado-web/static/js/main.50cf7edd.chunk.js"
+  },
+  {
+    "revision": "f61608a40f6b25ada884",
+    "url": "/tere-agasalhado-web/static/js/runtime-main.c33cc3c3.js"
+  },
+  {
+    "revision": "db34ba2f33a1c6d30b1a37b72ab4d0d4",
+    "url": "/tere-agasalhado-web/static/media/landing.db34ba2f.png"
+  }
+]);
