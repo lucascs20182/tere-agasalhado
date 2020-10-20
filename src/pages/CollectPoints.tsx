e3293bef019e92b8854d8db591799c7dfc86ae65
lucascs20182 <lucascs20182@gmail.com>
2020-10-20 10:26:54 -0300

    added new page of collect donations points

--- src/pages/CollectPoints.tsx
+++ src/pages/CollectPoints.tsx
@@ -1,8 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+
+import logoImg from '../assets/mark.png';
+
+import '../styles/pages/collectPoints.css';
 
 function CollectPoints() {
     return (
-        <h1>Hello World!</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={logoImg} alt="Logomarca do Terê Agasalhado" />
+
+                    <h2>Escolha um ponto de coleta no mapa</h2>
+                    <p>Muitas pessoas estão esperando a sua doação :)</p>
+                </header>
+                <footer>
+                    <strong>Teresópolis</strong>
+                    <span>Rio de Janeiro</span>
+                </footer>
+            </aside>
+
+            <Map center={[-22.4151909, -42.976899]} zoom={15} 
+                style={{ width: '100%', height: '100%' }}>
+                    <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            </Map>
+
+            <Link to="" className="create-collect-point">
+                <FiPlus size={32} color="#fff" />
+            </Link>
+        </div>
     )
 }
 
